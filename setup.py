972064cc88286bffd5077cb5fdfbb800bd4dafff
Seth M Morton <seth.m.morton@gmail.com>
2012-08-05 20:45:28 -0400

    Added a __version__ attribute that only needs to be set once

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from distutils.core import setup
 from os.path import join
 
+# Read the _version.py file for the module version number
+import re
+VERSIONFILE = 'natsort.py'
+with open(VERSIONFILE, "rt") as fl:
+    dummy = fl.read()
+    versionstring = fl.read().strip()
+m = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", versionstring)
+if m:
+    version = m.group(1)
+else:
+    s = "Unable to locate version string in {0}"
+    raise RuntimeError (s.format(VERSIONFILE))
+
 setup(name='natsort',
-      version='1.2',
+      version=version,
       author='Seth M. Morton',
       author_email='drtuba78@gmail.com',
       url='https://github.com/SethMMorton/natsort',
