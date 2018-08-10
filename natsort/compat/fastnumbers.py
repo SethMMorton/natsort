d335be9fd505f472671ad4e93cd0840e153d6a6e
Seth M Morton <seth.m.morton@gmail.com>
2018-08-10 17:25:49 -0400

    Refactor fastnumbers version checking.
    The new implementation has only one import statement, and
    blocks PyCharm from warning against fastnumbers not being installed
    (since it is optional this is preferred).

--- natsort/compat/fastnumbers.py
+++ natsort/compat/fastnumbers.py
@@ -10,11 +10,11 @@ from distutils.version import StrictVersion
 # If the user has fastnumbers installed, they will get great speed
 # benefits. If not, we use the simulated functions that come with natsort.
 try:
-    from fastnumbers import fast_float, fast_int
-    import fastnumbers
+    # noinspection PyPackageRequirements
+    from fastnumbers import fast_float, fast_int, __version__ as fn_ver
 
     # Require >= version 0.7.1.
-    if StrictVersion(fastnumbers.__version__) < StrictVersion("0.7.1"):
+    if StrictVersion(fn_ver) < StrictVersion("0.7.1"):
         raise ImportError  # pragma: no cover
 except ImportError:
     from natsort.compat.fake_fastnumbers import fast_float, fast_int
